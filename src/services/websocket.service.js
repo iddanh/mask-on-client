4f71dd579fac4f85d1805d87ca39158239ea0573
Iddan <iddanha@ex.co>
2022-05-01 19:57:49 +0300

    Get server from query params

--- src/services/websocket.service.js
+++ src/services/websocket.service.js
@@ -1,7 +1,11 @@
 let socket;
 
 const connect = (callback) => {
-  socket = new WebSocket('ws://localhost:8080');
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  socket = new WebSocket(params.server || 'ws://localhost:8080');
 
   // Listen for messages
   socket.addEventListener('message', (event) => {
